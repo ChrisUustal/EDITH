b6f29f77bde218e9780ac3126ad570fc2eda4065
ChrisUustal <christopheruustal@gmail.com>
2021-12-15 00:45:30 -0600

    added frame rate limiter

--- edith/edith.ino
+++ edith/edith.ino
@@ -2,16 +2,21 @@
 #include "display.h"
 
 //#define SER_DEBUG
-
+unsigned long timer_ref;
+const unsigned long timer_offset = 40;
 
 void setup() {
 
   display_setup();
+  timer_ref = millis();
 
 }
 
 void loop() {
 
-  display_loop();
-
+  if(millis() >= timer_ref + timer_offset){
+    timer_ref = millis();
+    display_loop();
+  }
+  
 }
